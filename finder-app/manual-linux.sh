862b92ae3ccff6d25d3cc80fa9d0944694378297
Dariusz <dariusz@Ideapad5Pro>
2025-02-11 21:30:35 +0100

    TODOs implemented, scripts adjusted, writer is working in quemu

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,24 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+
+    make -j12 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    
+    make -j12 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+
+
 fi
 
 echo "Adding the Image in outdir"
+
+    cp  ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/
+    cp  ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image.gz ${OUTDIR}/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +63,11 @@ then
 fi
 
 # TODO: Create necessary base directories
+    mkdir -p rootfs
+    cd rootfs
+    mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+    mkdir -p usr/bin usr/lib usr/sbin
+    mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +76,44 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+    make distclean
+    make defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+    make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+    cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+    cp /home/dariusz/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64
+    cp /home/dariusz/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
+    cp /home/dariusz/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
+    cp /home/dariusz/arm-cross-compiler/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
 
 # TODO: Make device nodes
+    sudo mknod -m 666 dev/null c 1 3
+    sudo mknod -m 600 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+    make clean -C /home/dariusz/assignment-1-dbo01/finder-app/
+    make CROSS_COMPILE=aarch64-none-linux-gnu- -C /home/dariusz/assignment-1-dbo01/finder-app/
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
-
+    cp -r /home/dariusz/assignment-1-dbo01/finder-app/. ${OUTDIR}/rootfs/home
+    cp -a /home/dariusz/assignment-1-dbo01/finder-app/conf/. ${OUTDIR}/rootfs/home
 # TODO: Chown the root directory
+    cd ${OUTDIR}/rootfs/
+    sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+    find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+    gzip -f ${OUTDIR}/initramfs.cpio
